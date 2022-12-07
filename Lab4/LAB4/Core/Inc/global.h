08b4e3f224214249b14d5be13d5f6b7deda7ae91
Leanhthi <leanhthi65.23@gmail.com>
2022-12-07 11:18:48 +0700

    init

---
+++ Lab4/LAB4/Core/Inc/global.h
@@ -0,0 +1,24 @@
+/*
+ * global.h
+ *
+ *  Created on: Sep 30, 2022
+ *      Author: thi
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#define INIT 0
+#define AUTO_RED 1
+#define AUTO_GREEN 2
+#define AUTO_YELLOW 3
+
+#define MAN_RED 11
+#define MAN_YELLOW 12
+#define MAN_GREEN 13
+extern int status;
+
+extern int global_time;
+void update_time();
+int get_time();
+#endif /* INC_GLOBAL_H_ */
---
+++ Lab4/LAB4/Core/Src/global.c
@@ -0,0 +1,19 @@
+/*
+ * global.c
+ *
+ *  Created on: Sep 30, 2022
+ *      Author: thi
+ */
+#include "global.h"
+
+int global_time = 0;
+int status = 0;
+
+void update_time()
+{
+	global_time +=10;
+}
+int get_time()
+{
+	return global_time;
+}
